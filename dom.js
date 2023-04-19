771a8cab874b402321a2a4306bc86a932205a408
varsha027 <varshagupta027@gmail.com>
2023-04-19 21:30:42 +0530

    final

--- dom.js
+++ dom.js
@@ -1,4 +1,12 @@
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[3]);
-items[3].getElementsByClassName.backgroundColor='green';
+//GET ELEMENT BY CLASS NAME
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[3]);
+// items[3].textContent="hello";
+// items[3].style.background="green";
+//GET ELEMENT BY TAGNAME
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[3]);
+li[3].textContent="hello";
+li[3].style.background="green";

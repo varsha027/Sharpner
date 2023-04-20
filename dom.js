42b7b992a250caa2a0a43bddc0c2cd37146b65ba
varsha027 <varshagupta027@gmail.com>
2023-04-20 12:38:33 +0530

    last

--- dom.js
+++ dom.js
@@ -5,8 +5,20 @@
 // items[3].textContent="hello";
 // items[3].style.background="green";
 //GET ELEMENT BY TAGNAME
-var li = document.getElementsByTagName('li');
-console.log(li);
-console.log(li[3]);
-li[3].textContent="hello";
-li[3].style.background="green";
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[3]);
+// li[3].textContent="hello";
+// li[3].style.background="green";
+///query selector
+var header = document.querySelector('#main-header');
+header.style.borderBottom="solid 4px #ccc"
+var input = document.querySelector('input');
+input.value="hiiii"
+var item = document.querySelector('.list-group-item:nth-child(2)');
+item.style.backgroundColor="green";
+var third = document.querySelector('.list-group-item:nth-child(3)');
+third.style.color="white";
+
+
+
